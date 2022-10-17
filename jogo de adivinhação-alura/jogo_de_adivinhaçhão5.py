e89b4e02a8fd96c71cce27d83ed74ee849734544
Davimteixeira <daviteixeira077@gmail.com>
2022-10-17 19:43:47 -0300

    colocando niveis 1, 2 e 3 no jogo

---
+++ jogo de adivinhação-alura/jogo_de_adivinhaçhão5.py
@@ -0,0 +1,47 @@
+import random
+
+
+print('---------------------------------')
+print('bem vindo ao jogo de adivinhação')
+print('---------------------------------')
+
+numero_secreto = random.randrange(1,101) 
+total_de_tentativas = 0
+
+print("qual o nivel de dificuldade ?")
+print("(1) fácil (2) médio (30) dificil")
+
+nivel = int(input("defina um nivel: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
+for rodada in range(1, total_de_tentativas + 1):
+    print(f"tentativa {rodada} de {total_de_tentativas}")
+    chute_str = input('digite um numero entre 1 e 100: ')
+    print('voce digitou', chute_str)
+    chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("você deve digitar um número entre 1 e 100")
+        continue
+    
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+         print("Você acertou!")
+         break
+    else:
+        if (maior):
+           print("Você errou! O seu chute foi maior que o número secreto.")   
+        elif (menor):
+           print("Você errou! O seu chute foi menor que o número secreto.")
+    if (total_de_tentativas == rodada):
+           print("FIM DE JOGO !!!!!")
+    
